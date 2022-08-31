d7b095eebf91a2365d39d0dd0b8b905a11c28af2
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-08-31 15:18:53 -0700

    commit tree 77191729eb3340091e02c2c46e3943dad49eb702

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+findMatching = (names, name) => {
+    return names.filter((allNames) => allNames.toLowerCase() === name.toLowerCase()
+    );
+}
+
+function fuzzyMatch (names, testName) {
+    return names.filter((nameMatch) => nameMatch.toLowerCase().indexOf(testName.toLowerCase()) === 0
+    );
+}
+
+function matchName (name, hometown) {
+    return name.filter((record) => record.name === hometown);
+}
